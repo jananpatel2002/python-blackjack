2e07ae41016ed728fa717fea19655d0e64803bfd
jananpatel2002 <70957608+jananpatel2002@users.noreply.github.com>
2022-10-20 22:38:16 +0000

    little commend and small changes for readability

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def blackjack():
     print(f"    The dealer is currently at a value of {dealerCardLists[0]}")
     choice = 'y'
     while (userValue < 21 and dealerValue < 21 and choice == 'y'):
-
+    
         choice = input("Would you like to pick another card? 'y' or 'n' ")
 
         if (choice == 'y' and userValue < 21):
@@ -58,7 +58,9 @@ def blackjack():
             print(f"    Your cards {userCardList}, current score {userValue}")
 
     
-    x = sum(addToDealer(dealerCardLists))
+    dealerCardLists = (addToDealer(dealerCardLists)) #Updates the cardlist for the dealer so that it has the max amount of hits
+    
+    x = sum(dealerCardLists)
 
     print(
         f"    Your final Hand is {userCardList} which makes a total of {userValue}"
@@ -68,13 +70,13 @@ def blackjack():
     )
 
     if (userValue == x and userValue <= 21 and x <= 21):
-        print("It was a Tie!")
+        print("\n        It was a Tie!")
     elif (userValue > 21):
-        print("You Lose! You went over 21")
+        print(" \n        You Lose! You went over 21")
     elif userValue < x and x <= 21 and userValue <= 21:
-        print("You Lose, the dealer has a higher number!")
+        print(" \n        You Lose, the dealer has a higher number!")
     else:
-        print("You Win!!!")
+        print(" \n        You Win!!!")
 
     if (input("\nWould you like to try again? Yes or No? ").lower() == 'yes'):
         clear()
@@ -82,5 +84,7 @@ def blackjack():
 
 
 ###  MAIN  ###
-
-blackjack()
+playBlackjack= input("Would you like to play a game of blackjack: Yes or No: ").lower()
+if playBlackjack=="yes":
+    
+    blackjack()
